7c4288467dc1a951d408bcca521b3479bcd2768a
hemavarshi-ui <nithi2865@gmail.com>
2025-05-19 16:20:21 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import {Sample}from "./component/Sample"
 
 
@@ -7,13 +8,14 @@ function App() {
   const myName="hema"
   const myFrom ="nagappattinam"
   const myRead ="reading book"
+  const[count,setCount]=useState(0)
 
   return (
     <>
     <h1> hello </h1> 
 
     <h1 className="text-30xl font-bold underline"> hello world</h1>
-
+    <button onClick = {()=> setCount (count+1)}> count {count}</button>n
     
     <Sample myName = {myName} myFrom = {myFrom} myRead = {myRead}/>
     </>
